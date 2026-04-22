3016668b779357f5a89dbe9e36a544ffe21bad44
quattro441 <pauyo441@gmail.com>
2026-04-22 20:59:01 +0200

    sec10-doc-proj env setup done!

---
+++ sec10-build-a-doc-assistant/ingestion.py
@@ -0,0 +1,36 @@
+import asyncio
+
+
+
+
+
+
+
+
+
+
+
+
+async def main():
+    """Main asyn function to orchestrate the entire process."""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ sec10-build-a-doc-assistant/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sec10-build-a-doc-assistant!")
+
+
+if __name__ == "__main__":
+    main()
